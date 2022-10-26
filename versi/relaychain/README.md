b86e2cb0833f4e3b4b1b181150815cc29cff76c3
BulatSaif <bulat@parity.io>
2022-10-26 19:29:19 +0300

    update versi readme.md file
    Signed-off-by: BulatSaif <bulat@parity.io>

--- versi/relaychain/README.md
+++ versi/relaychain/README.md
@@ -1,4 +1,19 @@
 # Command to generate chainspec:
 `
- 1. Run: `docker run docker.io/paritypr/polkadot-debug:5635-b2cb08a1 build-spec --chain versi-staging --raw  > chainspec.json`
- 2. Update `name`, `id`, `bootnodes`. 
+ 1. Run following script to generate chainspec.json:
+```
+DOCKER_IMAGE=docker.io/paritypr/polkadot-debug:5635-b2cb08a1
+
+# 1. generate base chainspec  
+docker run $DOCKER_IMAGE  build-spec --chain versi-staging  > ./versi-base.json
+
+# 2. Merge versi-base.json and versi-override.json
+jq  -s '.[0] * .[1]'  versi-base.json versi-override.json | sed 's/1e+18/1000000000000000000/' > ./chainspec-nonraw.json
+
+# 3. Convert to raw 
+docker run --rm -v $(pwd):/dir -w /dir  $DOCKER_IMAGE  build-spec --chain /dir/chainspec-nonraw.json --raw  > ./chainspec.json
+
+# 4. Clean up
+rm versi-base.json
+
+```
--- versi/relaychain/prepare-test-net.sh
+++ versi/relaychain/prepare-test-net.sh
@@ -1,64 +1,82 @@
 #!/usr/bin/env bash
-
-# Source: https://github.com/paritytech/polkadot/blob/master/scripts/prepare-test-net.sh
-# Usage:
-# SECRET="***VALIDATORS_ROOT_SEED***" ./prepare-test-net.sh 7
-
 set -e
+###
+# Script to generate part of chainspec with keys and accounts.
+###
+
 
 if [ "$#" -ne 1 ]; then
 	echo "Please provide the number of initial validators!"
+	echo "Usage: $0 8 > ./versi-override.json"
+	echo "export SUDO_SEED= # seed "
+	echo "export VALIDATORS_ROOT_SEED= #base seed to derive from"
+	echo "./prepare-test-net.sh 8 > ./versi-override.json"
+	echo "NOTE: Update beefy manually subkey can not generate such key"
 	exit 1
 fi
 
-generate_account_id() {
-	subkey inspect ${3:-} ${4:-} "$SECRET//$1//$2" | grep "Account ID" | awk '{ print $3 }'
-}
+PREFIX=versi-bootnode-
 
 generate_address() {
-	subkey inspect ${3:-} ${4:-} "$SECRET//$1//$2" | grep "SS58 Address" | awk '{ print $3 }'
+	subkey inspect ${3:-} ${4:-} "$VALIDATORS_ROOT_SEED//$PREFIX$1//$2" | grep "SS58 Address" | awk '{ print $3 }'
 }
 
-generate_public_key() {
-	subkey inspect ${3:-} ${4:-} "$SECRET//$1//$2" | grep "Public" | awk '{ print $4 }'
-}
+V_NUM=$(($1 - 1))
 
-generate_address_and_public_key() {
-	ADDRESS=$(generate_address $1 $2 $3)
-	PUBLIC_KEY=$(generate_public_key $1 $2 $3)
+BALANCES='\"balances\":\n {"balances": [\n'
+SESSION='"session": {\n   "keys": [\n'
 
-	printf "//$ADDRESS\nhex![\"${PUBLIC_KEY#'0x'}\"].unchecked_into(),"
-}
-
-generate_address_and_account_id() {
-	ACCOUNT=$(generate_account_id $1 $2 $3)
-	ADDRESS=$(generate_address $1 $2 $3)
-	if ${4:-false}; then
-		INTO="unchecked_into"
-	else
-		INTO="into"
-	fi
-
-	printf "//$ADDRESS\nhex![\"${ACCOUNT#'0x'}\"].$INTO(),"
-}
-
-V_NUM=$1
-
-AUTHORITIES=""
+SUDO_ADDRESS=$(subkey inspect $SUDO_SEED | grep "SS58 Address" | awk '{ print $3 }')
+BALANCES+="  [\"${SUDO_ADDRESS}\", 1000000000000000000],\n"
 
 for i in $(seq 0 $V_NUM); do
-  NAME=versi-bootnode-$i
-	AUTHORITIES+="(\n//$NAME\n"
-	AUTHORITIES+="$(generate_address_and_account_id $NAME stash)\n"
-	AUTHORITIES+="$(generate_address_and_account_id $NAME controller)\n"
-	AUTHORITIES+="$(generate_address_and_account_id $NAME babe '--scheme sr25519' true)\n"
-	AUTHORITIES+="$(generate_address_and_account_id $NAME grandpa '--scheme ed25519' true)\n"
-	AUTHORITIES+="$(generate_address_and_account_id $NAME im_online '--scheme sr25519' true)\n"
-	AUTHORITIES+="$(generate_address_and_account_id $NAME para_validator '--scheme sr25519' true)\n"
-	AUTHORITIES+="$(generate_address_and_account_id $NAME para_assignment '--scheme sr25519' true)\n"
-	AUTHORITIES+="$(generate_address_and_account_id $NAME authority_discovery '--scheme sr25519' true)\n"
-	AUTHORITIES+="$(generate_address_and_public_key $NAME beefy '--scheme ecdsa' true)\n"
-	AUTHORITIES+="),\n"
-done
+  if [ $i -eq $V_NUM ]
+  then
+    LAST=""
+  else
+    LAST=','
+  fi
+  STASH=$(generate_address $i stash)
 
-printf "$AUTHORITIES"
+  BALANCES+="  [\"${STASH}\", 100000000000000]${LAST}\n"
+
+	SESSION+="[\n"
+	SESSION+="  \"${STASH}\",\n"
+	SESSION+="  \"${STASH}\",\n"
+	SESSION+="  {\n"
+	SESSION+="    \"grandpa\": \"$(generate_address $i grandpa '--scheme ed25519')\",\n"
+	SESSION+="    \"babe\": \"$(generate_address $i babe '--scheme sr25519')\",\n"
+	SESSION+="    \"im_online\": \"$(generate_address $i im_online '--scheme sr25519')\",\n"
+	SESSION+="    \"para_validator\": \"$(generate_address $i para_validator '--scheme sr25519')\",\n"
+	SESSION+="    \"para_assignment\": \"$(generate_address $i para_assignment '--scheme sr25519')\",\n"
+	SESSION+="    \"authority_discovery\": \"$(generate_address $i authority_discovery '--scheme sr25519')\",\n"
+	SESSION+="    \"beefy\": \"$(generate_address $i beefy '--scheme ecdsa')\"\n" #  <= producing wrong key
+	SESSION+="  }\n]${LAST}\n"
+done
+BALANCES+="]}"
+SESSION+='  ]\n}\n'
+
+CHAINSPEC='{
+   "name": "Versi",
+   "id": "versi_v1_10",
+   "bootNodes": [
+      "/dns/versi-bootnode-0.parity-versi.parity.io/tcp/30333/p2p/12D3KooWMHy2qkBrpZVv11No8Ysfckab3cCkf1GWMTEVeAz3SQrC",
+      "/dns/versi-bootnode-1.parity-versi.parity.io/tcp/30333/p2p/12D3KooWEQShxW9m9MVecXK5FHq8jaqAqukcjVTkgmnhH61LDMNu",
+      "/dns/versi-bootnode-2.parity-versi.parity.io/tcp/30333/p2p/12D3KooWR1N3e5nwJaqAEn27ydg6dLian7LJYuHB6rx6rUuZWg3u",
+      "/dns/versi-bootnode-3.parity-versi.parity.io/tcp/30333/p2p/12D3KooWJRjtuu5xzjU6WSKYcWC326i8HtVQsyYAVa6x2Ky2hukY",
+      "/dns/versi-bootnode-4.parity-versi.parity.io/tcp/30333/p2p/12D3KooWHKy3VggzwJTMVuiAYJK3ARtfCYp8qYEjicHNF6py5TGG",
+      "/dns/versi-bootnode-5.parity-versi.parity.io/tcp/30333/p2p/12D3KooWLQNS8ULhha5LLTWnSV3DHeXBfUFqr8vjNPGM9SAzBdgu",
+      "/dns/versi-bootnode-6.parity-versi.parity.io/tcp/30333/p2p/12D3KooWGsHmcZFhu64ue51SzWNdWVaBdjpKvrURkRHNcX4sLAhh",
+      "/dns/versi-bootnode-7.parity-versi.parity.io/tcp/30333/p2p/12D3KooWDbKdQoWsjDwegEc2XarYWaiTogyVkixE52nAiTXtiEVp"
+   ],
+   "genesis": {
+      "runtime": {
+         "runtime_genesis_config": {
+           '"$BALANCES, ${SESSION}"'
+         }
+      }
+   }
+}
+'
+
+printf "$CHAINSPEC" | jq | sed 's/1e+18/1000000000000000000/'
