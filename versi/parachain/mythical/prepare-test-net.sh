224cf834a531e0869edb0cad4aea31db91ddd877
BulatSaif <bulat@parity.io>
2023-05-23 17:18:59 +0100

    Add mythical parachain
    Signed-off-by: BulatSaif <bulat@parity.io>

---
+++ versi/parachain/mythical/prepare-test-net.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+set -e
+###
+# Script to generate part of chainspec with keys and accounts.
+###
+
+
+if [ -z "$VALIDATORS_ROOT_SEED" ]; then
+	echo "Please Set VALIDATORS_ROOT_SEED!"
+	echo "Usage:"
+	echo "export VALIDATORS_ROOT_SEED= #base seed to derive from"
+	echo "$0"
+	exit 1
+fi
+PREFIX=versi-mythical-collator-node-
+
+generate_address() {
+	subkey inspect --network geek "$VALIDATORS_ROOT_SEED//collator//$PREFIX$1" | grep "SS58 Address" | awk '{ print $3 }'
+}
+
+
+V_NUM=3 # 4 collators
+BOOTNODES='\"bootNodes\": [\n'
+BOOTNODES_KEY_LIST=''
+INVULNERABLES='"invulnerables": [\n'
+SESSION='"session": {\n   "keys": [\n'
+
+for i in $(seq 0 $V_NUM); do
+  if [ $i -eq $V_NUM ]
+  then
+    LAST=""
+  else
+    LAST=','
+  fi
+
+  NODEID_KEY=$(subkey generate-node-key)
+  NODEID=$(subkey inspect-node-key --file <( echo -n $NODEID_KEY ))
+  BOOTNODES_KEY_LIST+="${PREFIX}nodekey-${i}: ${NODEID_KEY}\n"
+  BOOTNODES+="\"/dns/${PREFIX}${i}.parity-versi.parity.io/tcp/30333/p2p/${NODEID}\"${LAST}"
+
+  STASH=$(generate_address $i)
+  INVULNERABLES+="  \"${STASH}\"${LAST}\n"
+
+	SESSION+="[\n"
+	SESSION+="  \"${STASH}\",\n"
+	SESSION+="  \"${STASH}\",\n"
+	SESSION+="  {\n"
+	SESSION+="    \"aura\": \"${STASH}\"\n"
+	SESSION+="  }\n]${LAST}\n"
+done
+SESSION+='  ]\n}\n'
+INVULNERABLES+="]"
+BOOTNODES+="]"
+
+CHAINSPEC='{
+  "name": "mythical",
+  "id": "mythical-versi",
+  '"${BOOTNODES}"',
+  "relay_chain": "versi",
+  "genesis": {
+    "runtime": {
+      "collatorSelection": {
+        '"${INVULNERABLES}"'
+      },
+      '"${SESSION}"'
+    }
+  }
+}
+'
+
+printf "$CHAINSPEC" | jq > ./mythical-override.json
+printf "${BOOTNODES_KEY_LIST}" | sed 's/-/_/g' > ./mythical_nodekey.yml
