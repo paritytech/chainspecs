c36daec8d365237abe346c2fa294e5052b902a26
BulatSaif <bulat@parity.io>
2023-09-20 15:44:18 +0400

    update versi chainspec  master-771c3fbd
    Signed-off-by: BulatSaif <bulat@parity.io>

--- versi/relaychain-pos/build-chainspec.sh
+++ versi/relaychain-pos/build-chainspec.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-TAG=master-12194445
+TAG=master-771c3fbd
 
 DOCKER_IMAGE="docker.io/paritypr/polkadot-debug:$TAG"
 echo $DOCKER_IMAGE
 docker run $DOCKER_IMAGE  build-spec --chain westend-staging  > ./versi-base.json
 
-# Merge versi-base.json and versi-override.json
-jq  -s '.[0] * .[1]'  versi-base.json versi-override.json | sed 's/1e+18/1000000000000000000/' > ./chainspec-plain.json
-
-# Temporary workaround: patch id to `westend`
-echo "$(jq '.id = "westend"' chainspec-plain.json)" > chainspec-plain.json
+# Merge versi-base.json and versi-override.json + Temporary workaround: patch id to `westend`
+jq  -s '.[0] * .[1]'  versi-base.json versi-override.json | jq '.id = "westend"' | sed 's/1e+18/1000000000000000000/' > ./chainspec-plain.json
 
 # Convert to raw chainspec
 docker run --rm -v $(pwd):/dir:z -w /dir $DOCKER_IMAGE build-spec --chain /dir/chainspec-plain.json --raw  > ./chainspec.json
