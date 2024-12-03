2c7cdce1434a787bf34b014579bd754fe54e30f9
BulatSaif <bulat@parity.io>
2024-12-03 10:06:34 +0100

    fix name

--- westend/parachain/penpal/prepare-test-net.sh
+++ westend/parachain/penpal/prepare-test-net.sh
@@ -26,7 +26,7 @@ echo "${NAME}_collator_root_seed: $SEED" | sed 's/-/_/g' > ${keys_file}
 echo -e "IMAGE=$IMAGE\nBASE_CHAIN=$BASE_CHAIN" > info.txt
 echo '{}' > override.json
 for i in `seq 0 $LAST_COLLATOR`; do
-  HOSTNAME=$NAME-node-${i}
+  HOSTNAME=$NAME-collator-node-${i}
   NODEID_KEY=$(subkey generate-node-key)
   echo "${NAME}_collator_nodekey_${i}: $NODEID_KEY" | sed 's/-/_/g' >> ${keys_file}
   ID=$(subkey inspect-node-key --file <( echo -n $NODEID_KEY ))
